013194cb0f3b0c4c7acd10f90120378b24ce73de
herhor67 <7432818+dawinaj@users.noreply.github.com>
2025-02-20 21:00:42 +0100

    segdisp16 partial

--- main/include/SegmentDisplay.h
+++ main/include/SegmentDisplay.h
@@ -61,45 +61,144 @@ namespace SegmentDisplay
 		switch (c)
 		{
 		case '0':
-			return 0 | ET::Top | ET::UpperRight | ET::LowerRight | ET::Bottom | ET::LowerLeft | ET::UpperLeft;
+			return static_cast<T>(0) | ET::Top | ET::UpperRight | ET::LowerRight | ET::Bottom | ET::LowerLeft | ET::UpperLeft;
 		case '1':
-			return 0 | ET::UpperRight | ET::LowerRight;
+			return static_cast<T>(0) | ET::UpperRight | ET::LowerRight;
 		case '2':
-			return 0 | ET::Top | ET::UpperRight | ET::Middle | ET::LowerLeft | ET::Bottom;
+			return static_cast<T>(0) | ET::Top | ET::UpperRight | ET::Middle | ET::LowerLeft | ET::Bottom;
 		case '3':
-			return 0 | ET::Top | ET::UpperRight | ET::Middle | ET::LowerRight | ET::Bottom;
+			return static_cast<T>(0) | ET::Top | ET::UpperRight | ET::Middle | ET::LowerRight | ET::Bottom;
 		case '4':
-			return 0 | ET::UpperLeft | ET::Middle | ET::UpperRight | ET::LowerRight;
+			return static_cast<T>(0) | ET::UpperLeft | ET::Middle | ET::UpperRight | ET::LowerRight;
 		case '5':
-			return 0 | ET::Top | ET::UpperLeft | ET::Middle | ET::LowerRight | ET::Bottom;
+			return static_cast<T>(0) | ET::Top | ET::UpperLeft | ET::Middle | ET::LowerRight | ET::Bottom;
 		case '6':
-			return 0 | ET::Top | ET::UpperLeft | ET::Middle | ET::LowerLeft | ET::LowerRight | ET::Bottom;
+			return static_cast<T>(0) | ET::Top | ET::UpperLeft | ET::Middle | ET::LowerLeft | ET::LowerRight | ET::Bottom;
 		case '7':
-			return 0 | ET::Top | ET::UpperRight | ET::LowerRight;
+			return static_cast<T>(0) | ET::Top | ET::UpperRight | ET::LowerRight;
 		case '8':
-			return 0 | ET::Top | ET::UpperRight | ET::LowerRight | ET::Bottom | ET::LowerLeft | ET::UpperLeft | ET::Middle;
+			return static_cast<T>(0) | ET::Top | ET::UpperRight | ET::LowerRight | ET::Bottom | ET::LowerLeft | ET::UpperLeft | ET::Middle;
 		case '9':
-			return 0 | ET::Top | ET::UpperRight | ET::LowerRight | ET::Bottom | ET::UpperLeft | ET::Middle;
+			return static_cast<T>(0) | ET::Top | ET::UpperRight | ET::LowerRight | ET::Bottom | ET::UpperLeft | ET::Middle;
+
 		case 'A':
 		case 'a':
-			return 0 | ET::Top | ET::UpperRight | ET::LowerRight | ET::LowerLeft | ET::UpperLeft | ET::Middle;
+			return static_cast<T>(0) | ET::Top | ET::UpperRight | ET::LowerRight | ET::LowerLeft | ET::UpperLeft | ET::Middle;
 		case 'B':
 		case 'b':
-			return 0 | ET::LowerRight | ET::Bottom | ET::LowerLeft | ET::UpperLeft | ET::Middle;
+			return static_cast<T>(0) | ET::LowerRight | ET::Bottom | ET::LowerLeft | ET::UpperLeft | ET::Middle;
 		case 'C':
 		case 'c':
-			return 0 | ET::Top | ET::Bottom | ET::LowerLeft | ET::UpperLeft;
+			return static_cast<T>(0) | ET::Top | ET::Bottom | ET::LowerLeft | ET::UpperLeft;
 		case 'D':
 		case 'd':
-			return 0 | ET::UpperRight | ET::LowerRight | ET::Bottom | ET::LowerLeft | ET::Middle;
+			return static_cast<T>(0) | ET::UpperRight | ET::LowerRight | ET::Bottom | ET::LowerLeft | ET::Middle;
 		case 'E':
 		case 'e':
-			return 0 | ET::Top | ET::Bottom | ET::LowerLeft | ET::UpperLeft | ET::Middle;
+			return static_cast<T>(0) | ET::Top | ET::Bottom | ET::LowerLeft | ET::UpperLeft | ET::Middle;
 		case 'F':
 		case 'f':
-			return 0 | ET::Top | ET::LowerLeft | ET::UpperLeft | ET::Middle;
+			return static_cast<T>(0) | ET::Top | ET::LowerLeft | ET::UpperLeft | ET::Middle;
+
 		default:
-			return 0; // Unknown character, return 0 |  0 (no segments on
+			return 0; // Unknown character, return static_cast<T>(0) |  0 (no segments on
+		}
+	}
+
+	// Function template to map a character to a 7-segment bit pattern
+	template <typename ET = Default16Segment, typename T = uint16_t>
+	constexpr T char_to_16seg(char c)
+	{
+		static_assert(std::is_integral<T>::value, "Return type must be an integer");
+		// static_assert(std::is_unsigned<T>::value, "Return type must be unsigned");
+
+		inline constexpr T operator|(T aggr, ET seg)
+		{
+			// return aggr | static_cast<T>(1 << static_cast<std::underlying_type_t>(seg));
+			return aggr | static_cast<T>(seg);
+		}
+
+		switch (c)
+		{
+		case '0':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperRight | ET::LowerRight | ET::BottomLeft | ET::BottomRight | ET::LowerLeft | ET::UpperLeft;
+		case '1':
+			return static_cast<T>(0) | ET::UpperRight | ET::LowerRight;
+		case '2':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperRight | ET::MiddleLeft | ET::MiddleRight | ET::LowerLeft | ET::BottomLeft | ET::BottomRight;
+		case '3':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperRight | ET::MiddleLeft | ET::MiddleRight | ET::LowerRight | ET::BottomLeft | ET::BottomRight;
+		case '4':
+			return static_cast<T>(0) | ET::UpperLeft | ET::MiddleLeft | ET::MiddleRight | ET::UpperRight | ET::LowerRight;
+		case '5':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperLeft | ET::MiddleLeft | ET::MiddleRight | ET::LowerRight | ET::BottomLeft | ET::BottomRight;
+		case '6':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperLeft | ET::MiddleLeft | ET::MiddleRight | ET::LowerLeft | ET::LowerRight | ET::BottomLeft | ET::BottomRight;
+		case '7':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperRight | ET::LowerRight;
+		case '8':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperRight | ET::LowerRight | ET::BottomLeft | ET::BottomRight | ET::LowerLeft | ET::UpperLeft | ET::MiddleLeft | ET::MiddleRight;
+		case '9':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperRight | ET::LowerRight | ET::BottomLeft | ET::BottomRight | ET::UpperLeft | ET::MiddleLeft | ET::MiddleRight;
+
+		case 'A':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperRight | ET::LowerRight | ET::LowerLeft | ET::UpperLeft | ET::MiddleLeft | ET::MiddleRight;
+		case 'B':
+			return static_cast<T>(0) | ET::LowerRight | ET::BottomLeft | ET::BottomRight | ET::LowerLeft | ET::UpperLeft | ET::MiddleLeft | ET::MiddleRight;
+		case 'C':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::BottomLeft | ET::BottomRight | ET::LowerLeft | ET::UpperLeft;
+		case 'D':
+			return static_cast<T>(0) | ET::UpperRight | ET::LowerRight | ET::BottomLeft | ET::BottomRight | ET::LowerLeft | ET::MiddleLeft | ET::MiddleRight;
+		case 'E':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::BottomLeft | ET::BottomRight | ET::LowerLeft | ET::UpperLeft | ET::MiddleLeft | ET::MiddleRight;
+		case 'F':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::LowerLeft | ET::UpperLeft | ET::MiddleLeft | ET::MiddleRight;
+
+		case 'G':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperLeft | ET::MiddleRight | ET::LowerLeft | ET::LowerRight | ET::BottomLeft | ET::BottomRight;
+		case 'H':
+			return static_cast<T>(0) | ET::UpperRight | ET::LowerRight | ET::LowerLeft | ET::UpperLeft | ET::MiddleLeft | ET::MiddleRight;
+		case 'I':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperCenter | ET::LowerCenter | ET::BottomLeft | ET::BottomRight;
+		case 'J':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperRight | ET::LowerRight | ET::BottomLeft | ET::BottomRight;
+		case 'K':
+			return static_cast<T>(0) | ET::LowerLeft | ET::UpperLeft | ET::MiddleLeft | ET::DiagUpperRight | ET::DiagLowerRight;
+
+		case 'L':
+			return static_cast<T>(0) | ET::BottomLeft | ET::BottomRight | ET::LowerLeft | ET::UpperLeft;
+		case 'M':
+			return static_cast<T>(0) | ET::UpperRight | ET::LowerRight | ET::LowerLeft | ET::UpperLeft | ET::DiagUpperRight | ET::DiagUpperLeft;
+		case 'N':
+			return static_cast<T>(0) | ET::UpperRight | ET::LowerRight | ET::LowerLeft | ET::UpperLeft | ET::DiagLowerRight | ET::DiagUpperLeft;
+		case 'O':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperRight | ET::LowerRight | ET::BottomLeft | ET::BottomRight | ET::LowerLeft | ET::UpperLeft;
+		case 'P':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperRight | ET::LowerLeft | ET::UpperLeft | ET::MiddleLeft | ET::MiddleRight;
+
+		case 'Q':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperRight | ET::LowerRight | ET::BottomLeft | ET::BottomRight | ET::LowerLeft | ET::UpperLeft | ET::DiagLowerRight;
+		case 'R':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperRight | ET::LowerLeft | ET::UpperLeft | ET::MiddleLeft | ET::MiddleRight | ET::DiagLowerRight;
+		case 'S':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::DiagUpperLeft | ET::MiddleLeft | ET::MiddleRight | ET::LowerRight | ET::BottomLeft | ET::BottomRight;
+		case 'T':
+			return static_cast<T>(0) | ET::TopLeft | ET::TopRight | ET::UpperCenter | ET::LowerCenter;
+		case 'U':
+
+		//
+		case 'V':
+
+		case 'W':
+
+		case 'X':
+
+		case 'Y':
+
+		case 'Z':
+
+		default:
+			return 0; // Unknown character, return static_cast<T>(0) |  0 (no segments on
 		}
 	}
 
